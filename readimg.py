30a38ec25052c933029a1a081e77365841a1c633
Ahmed Badra <42746335+badra069@users.noreply.github.com>
2021-04-24 22:59:11 +0200

    new imgread

--- readimg.py
+++ readimg.py
@@ -1,8 +1,3 @@
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-  
-# Read Images
-img = mpimg.imread('photo1.png')
-  
-# Output Images
-plt.imshow(img)
+from PIL import Image                                                                                
+img = Image.open('photo1.png')
+img.show() 
