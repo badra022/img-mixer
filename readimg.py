226fe54f66bf732d09d047b9b4ec59be448e6c0a
Hassan Hosni Abdel_Aleem <61256865+HassanHosniAbdelalem@users.noreply.github.com>
2021-04-24 23:33:22 +0200

    read and show data

--- readimg.py
+++ readimg.py
@@ -1,3 +1,15 @@
-from PIL import Image                                                                                
-img = Image.open('photo1.png')
-img.show() 
+from matplotlib import image
+from matplotlib import pyplot                                                                              
+from scipy.fft import irfft , rfft, rfftfreq
+import numpy as np 
+  
+
+# load image as pixel array
+image = image.imread('photo1.png')
+# summarize shape of the pixel array
+print(type(image))
+print(image.shape)
+# display the array of pixels as an image
+pyplot.imshow(image)
+pyplot.show()
+
