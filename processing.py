33bbd12909050c73c61aa6c04ba199c47bd79c27
Ahmed Badra <42746335+badra069@users.noreply.github.com>
2021-04-25 01:23:16 +0200

    trying to fix ft bug

--- processing.py
+++ processing.py
@@ -18,16 +18,13 @@ from matplotlib import image
 from matplotlib import pyplot                                                                              
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import rfft, rfftfreq, irfft
+from scipy.fft import rfft, rfftfreq, irfft, fft2, ifft2
 import cmath
+from PIL import Image
 
 # load image as pixel array
 image1 = image.imread('photo1.jpeg')
 image2 = image.imread('photo2.jpeg')
-# make a 1-dimensional view of arr
-image1 = image1.ravel()
-# make a 1-dimensional view of arr
-image2 = image2.ravel()
 
 print("for image1: ")
 print(type(image1))
@@ -37,25 +34,35 @@ print(type(image2))
 print(image2.shape)
 
 # getting fft of the signal and subtracting amplitudes and phases (1)
-rfft_coeff1 = rfft(image1)
-rfft_coeff2 = rfft(image2)
+rfft_coeff1 = fft2(image1)
+rfft_coeff2 = fft2(image2)
 signal_rfft_Coeff_abs1 = np.abs(rfft_coeff1)
 signal_rfft_Coeff_angle1 = np.angle(rfft_coeff1)
+signal_rfft_Coeff_real1 = np.real(rfft_coeff1)
+signal_rfft_Coeff_img1 = np.imag(rfft_coeff1)
+print(signal_rfft_Coeff_real1.shape)
+print(signal_rfft_Coeff_abs1.shape)
 
 # getting fft of the signal and subtracting amplitudes and phases (2)
 signal_rfft_Coeff_abs2 = np.abs(rfft_coeff2)
 signal_rfft_Coeff_angle2 = np.angle(rfft_coeff2)
+signal_rfft_Coeff_real2 = np.real(rfft_coeff2)
+signal_rfft_Coeff_img2 = np.imag(rfft_coeff2)
+print(signal_rfft_Coeff_real2.shape)
+print(signal_rfft_Coeff_abs2.shape)
 
-# constructing fft coefficients again (from amplitudes and phases) after processing the amplitudes
-new_rfft_coeff1 = np.zeros((len(rfft_coeff1),), dtype=complex)
-for pixel in range(len(rfft_coeff1)):
-    try:
-        new_rfft_coeff1[pixel]= signal_rfft_Coeff_abs2[pixel]*cmath.exp(1j * signal_rfft_Coeff_angle2[pixel])
-    except:
-        pass
 
-# constructing the new signal from the fft coeffs by inverse fft
-new_image1 = irfft(new_rfft_coeff1).reshape(433, 640, 3)
-# display the array of pixels as an image
-pyplot.imshow(new_image1)
-pyplot.show()
+# # # constructing fft coefficients again (from amplitudes and phases) after processing the amplitudes
+# # new_rfft_coeff1 = np.zeros((len(rfft_coeff1),), dtype=complex)
+# # for pixel in range(len(rfft_coeff1)):
+# #     try:
+# #         new_rfft_coeff1[pixel]= signal_rfft_Coeff_abs2[pixel]*cmath.exp(1j * signal_rfft_Coeff_angle2[pixel])
+# #     except:
+# #         pass
+
+# # constructing the new signal from the fft coeffs by inverse fft
+# new_image1 = irfft(new_rfft_coeff1)
+# # new_image1 = Image.fromarray(new_image1, 'RGB')
+# # display the array of pixels as an image
+# pyplot.imshow(new_image1.reshape(433, 640, 3))
+# pyplot.show()
