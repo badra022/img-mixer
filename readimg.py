4e56682f33fa59566dd3565c495214b3d34f093c
Hassan Hosni Abdel_Aleem <61256865+HassanHosniAbdelalem@users.noreply.github.com>
2021-04-25 23:55:47 +0200

    finish proccessing final

--- readimg.py
+++ readimg.py
@@ -42,23 +42,45 @@ for i in range (x):
     for g in range(y):
         for k in range (z):
             anglearrar[i,g,k]=0
-def mix_amb_ang(amb ,angle ):
+def mix_amb_ang(make1,amb ,angle,ratio1 ):
     combined1 = np.multiply(np.abs(amb), np.exp(1j*np.angle(angle)))
-    return combined1 
-def make_amb1 (angle):
-    combined2 = np.multiply(amplarray, np.exp(1j*np.angle(angle)))
-    return combined2
-def make_phase0 (amblitude ):
-    combined3 = np.multiply(np.abs(amblitude), np.exp(1j*anglearrar))
-    return combined3
-def join_real_imag(real,ima):
-    combined4=np.real(real) +1j *np.imag(ima)
-    return combined4 
+    p=make1
+    for i in range (int(x*ratio1)):
+        for g in range(y):
+            for k in range (z):
+                 p[i,g,k]=combined1[i,g,k]
     
-combined1=mix_amb_ang(photo1,photo1)
-combined2 = make_amb1 (photo1) 
-combined3=make_phase0(photo1) 
-combined4= join_real_imag (photo1,photo1)
+    return p
+def make_amb1 (make2,angle2,ratio2):
+    combined2 = np.multiply(amplarray, np.exp(1j*np.angle(angle2)))
+    o=make2
+    for i in range (int(x*ratio2)):
+        for g in range(y):
+            for k in range (z):
+                 o[i,g,k]=combined2[i,g,k]
+    return o
+def make_phase0 (make3,amblitude,ratio3 ):
+    combined3 = np.multiply(np.abs(amblitude), np.exp(1j*anglearrar))
+    r=make3
+    for i in range (int(x*ratio3)):
+        for g in range(y):
+            for k in range (z):
+                 r[i,g,k]=combined3[i,g,k]
+    return r
+def join_real_imag(make4,real,ima,ratio4):
+    combined4=np.real(real) +1j *np.imag(ima)
+    l=make4
+    for i in range (int(x*ratio4)):
+        for g in range(y):
+            for k in range (z):
+                 l[i,g,k]=combined4[i,g,k]
+
+    return l
+
+#combined1=mix_amb_ang(photo2,photo1,photo2,.5)
+#combined2 = make_amb1 (photo1,photo1,.5) 
+#combined3=make_phase0(photo2,photo2,.5) 
+combined4= join_real_imag (photo1,photo1,photo2,.5)
 # getting the real pixel values of the output image in the spatial domain
 imgCombined = np.real(ifft2(combined4))
 
